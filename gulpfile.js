edf632dd2c3a02512a21e6bea6ae2a7ac09864f9
Andrew Clinton <ddrew.clinton@gmail.com>
2017-01-18 16:20:47 -0500

    asset config stuffs

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const elixir = require('laravel-elixir');
+
+elixir((mix) => {
+	mix.styles([
+			'./bower_components/material-design-iconic-font/dist/css/material-design-iconic-font.min.css',
+			'./bower_components/animate.css/animate.min.css',
+			'./bower_components/select2/dist/css/select2.min.css',
+			'./bower_components/slick-carousel/slick/slick.css',
+			'./bower_components/nouislider/distribute/nouislider.min.css'
+		], 'resources/assets/css/build/vendor.css')
+		.less('app.less', 'resources/assets/css/build/app.css')
+		.styles([
+			'resources/assets/css/build/vendor.css',
+			'resources/assets/css/build/app.css'
+		], 'public/css/app.css')
+		.webpack('app.js');
+});
