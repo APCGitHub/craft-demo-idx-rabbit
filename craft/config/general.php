ed52f119ba8933fcdb54fc3a7a99da7e059bd854
Andrew Clinton <ddrew.clinton@gmail.com>
2017-01-18 12:11:07 -0500

    settings update

--- craft/config/general.php
+++ craft/config/general.php
@@ -8,26 +8,29 @@
  */
 
 return array(
+    '*' => array(
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 0,
 
-    // Base site URL
-    'siteUrl' => null,
+        // Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
+        'enableCsrfProtection' => true,
 
-    // Environment-specific variables (see https://craftcms.com/docs/multi-environment-configs#environment-specific-variables)
-    'environmentVariables' => array(),
+        // Whether "index.php" should be visible in URLs (true, false, "auto")
+        'omitScriptNameInUrls' => 'auto',
 
-    // Default Week Start Day (0 = Sunday, 1 = Monday...)
-    'defaultWeekStartDay' => 0,
+        // Control Panel trigger word
+        'cpTrigger' => 'admin',
+    ),
 
-    // Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
-    'enableCsrfProtection' => true,
+    '.dev' => array(
+        'siteUrl' => 'http://craft.idxrabbit.dev',
+        'environmentVariables' => array(),
+        'devMode' => true,
+    ),
 
-    // Whether "index.php" should be visible in URLs (true, false, "auto")
-    'omitScriptNameInUrls' => 'auto',
-
-    // Control Panel trigger word
-    'cpTrigger' => 'admin',
-
-	// Dev Mode (see https://craftcms.com/support/dev-mode)
-	'devMode' => false,
-
+    '.com' => array(
+        'siteUrl' => 'http://craft.idxrabbit.com',
+        'environmentVariables' => array(),
+        'devMode' => false,
+    )    
 );
