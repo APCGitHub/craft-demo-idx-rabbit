7b007d163cc94d416e4bc7ed5bac56db88c76eac
Andrew Clinton <ddrew.clinton@gmail.com>
2017-01-25 16:34:33 -0500

    pull in new logics

--- resources/assets/js/components/bootstrap.js
+++ resources/assets/js/components/bootstrap.js
@@ -1,8 +1,10 @@
 /* Inline template logics */
 require('./navSearch');
 require('./listings/listings.show');
+require('./actionHeader');
 
 /* Vue components */
 Vue.component('listingMap', require('./listings/ListingMap.vue'));
 Vue.component('listingFav', require('./listings/ListingFav.vue'));
+Vue.component('listingsMap', require('./listings/ListingsMap.vue'));
 Vue.component('slider', require('./form/Slider.vue'));
