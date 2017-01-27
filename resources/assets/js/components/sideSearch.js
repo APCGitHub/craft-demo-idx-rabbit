1ef13c22493a4b8745ca8ecf798b0a41d4b1d064
Andrew Clinton <ddrew.clinton@gmail.com>
2017-01-27 13:36:56 -0500

    finished updating hte side nav

--- resources/assets/js/components/sideSearch.js
+++ resources/assets/js/components/sideSearch.js
@@ -5,10 +5,26 @@ Vue.component('sideSearch', {
 			price: {
 				min: 0,
 				max: 10000000
+			},
+			footprint: {
+				min: 0,
+				max: 10000
+			},
+			lot: {
+				min: 0,
+				max: 100000
 			}
 		}
 	},
 	methods: {
+		priceChanged(val) {
+			
+		},
+		footprintChanged(val) {
 
+		},
+		lotChanged(val) {
+			
+		}
 	}
 });
