52e67e7849810e82d1dca39925808754cdcf3775
Andrew Clinton <ddrew.clinton@gmail.com>
2017-01-30 11:21:30 -0500

    side search is working

--- resources/assets/js/components/sideSearch.js
+++ resources/assets/js/components/sideSearch.js
@@ -16,15 +16,42 @@ Vue.component('sideSearch', {
 			}
 		}
 	},
-	methods: {
-		priceChanged(val) {
-			
-		},
-		footprintChanged(val) {
+	created() {
+		$(document).ready(() => {
+			$('#advanced-side-search-form').submit((e) => {
+				e.preventDefault();
+				e.stopImmediatePropagation();
 
+				$('#advanced-side-search-form #property_bedrooms_total').val($('#advanced-side-search-form input[name="advanced-search-beds"]:checked').val());
+				$('#advanced-side-search-form #property_baths_full').val($('#advanced-side-search-form input[name="advanced-search-baths"]:checked').val());
+				$('#advanced-side-search-form #property_garage_spaces').val($('#advanced-side-search-form input[name="advanced-search-garage"]:checked').val());
+				$('#advanced-side-search-form #property_year_built').val($('#advanced-side-search-form #property-year-built').val());
+
+				$('#advanced-side-search-form')[0].submit();
+			});
+		});
+	},
+	methods: {
+		priceChanged(min, max) {
+			let ps = this.$el.querySelector('#property_price_start');
+			let pu = this.$el.querySelector('#property_price_end');
+
+			ps.value = min;
+			pu.value = max;
 		},
-		lotChanged(val) {
-			
+		footprintChanged(min, max) {
+			let pu = this.$el.querySelector('#property_area_upper');
+			let pl = this.$el.querySelector('#property_area_lower');
+
+			pu.value = min;
+			pl.value = max;
+		},
+		lotChanged(min, max) {
+			let pu = this.$el.querySelector('#property_lot_upper');
+			let pl = this.$el.querySelector('#property_lot_lower');
+
+			pu.value = min;
+			pl.value = max;
 		}
 	}
 });
