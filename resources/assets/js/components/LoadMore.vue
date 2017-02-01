fb89eba627ed8cce1a6d43f8aa0271bdf7cde846
Andrew Clinton <ddrew.clinton@gmail.com>
2017-02-01 17:00:44 -0500

    new load more component

---
+++ resources/assets/js/components/LoadMore.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="load-more text-center">
+		<a href="" @click.prevent="fetch" class="waves-effect">&nbsp;<i class="zmdi zmdi-refresh"></i>Load More</a>
+	</div>
+</template>
+
+<script type="text/babel">
+	export default {
+		props: ['endpoint'],
+		data() {
+			return {
+
+			};
+		},
+		methods: {
+			fetch() {
+				let url = this.endpoint;
+
+				this.$http.get(url).then(res => {
+
+				}).catch(err => {
+
+				});
+			}
+		}
+	}
+</script>
--- resources/assets/js/components/bootstrap.js
+++ resources/assets/js/components/bootstrap.js
@@ -10,3 +10,4 @@ Vue.component('listingMap', require('./listings/ListingMap.vue'));
 Vue.component('listingFav', require('./listings/ListingFav.vue'));
 Vue.component('listingsMap', require('./listings/ListingsMap.vue'));
 Vue.component('slider', require('./form/Slider.vue'));
+Vue.component('loadMore', require('./LoadMore.vue'));
