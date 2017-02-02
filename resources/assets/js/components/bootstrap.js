0cca5dfa5030fd918b82529cfc111d437447659f
Andrew Clinton <ddrew.clinton@gmail.com>
2017-02-02 15:49:42 -0500

    refactory of bootstrap

--- resources/assets/js/components/bootstrap.js
+++ resources/assets/js/components/bootstrap.js
@@ -1,13 +1,12 @@
 /* Inline template logics */
 require('./navSearch');
-require('./listings/listings.show');
 require('./actionHeader');
 require('./sideSearch');
-require('./listings/lead.form');
+
 
 /* Vue components */
-Vue.component('listingMap', require('./listings/ListingMap.vue'));
-Vue.component('listingFav', require('./listings/ListingFav.vue'));
-Vue.component('listingsMap', require('./listings/ListingsMap.vue'));
 Vue.component('slider', require('./form/Slider.vue'));
 Vue.component('loadMore', require('./LoadMore.vue'));
+
+/* Pull in other directories */
+require('./listings/bootstrap');
