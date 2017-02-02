ea9199a0e37cc52ece13d179b819be2b08a1a747
Andrew Clinton <ddrew.clinton@gmail.com>
2017-02-02 15:48:13 -0500

    load more component is working

--- resources/assets/js/components/LoadMore.vue
+++ resources/assets/js/components/LoadMore.vue
@@ -6,20 +6,70 @@
 
 <script type="text/babel">
 	export default {
-		props: ['endpoint'],
+		props: {
+			searchId: Number,
+			orderBy: String,
+			endpoint: String, 
+			startPage: {
+				type: Number,
+				default: 1
+			},
+			per: {
+				type: Number,
+				default: 12
+			}
+		},
 		data() {
 			return {
-
+				page: this.startPage
 			};
 		},
 		methods: {
 			fetch() {
-				let url = this.endpoint;
+				let url = this.endpoint + '?';
+
+				//If hitting search endpoint
+				if(this.searchId) {
+					url += `search_id=${this.searchId}&`;
+				}
+
+				//Build ODATA query
+				let query = '';
+
+				if(this.page) {
+					query += `$skip=${this.page * this.per}`;
+				}
+
+				if(this.per) {
+					if(this.page)
+						query += '&';
+
+					query += `$top=${this.per}`;
+				}
+
+				if(this.orderBy) {
+					if(this.page && this.per) {
+						query += '&';
+					}
+
+					query += `$orderby=${this.orderBy}`;
+				}
+
+				//Encode the query as it is a nested query param
+				query = encodeURIComponent(query);
+
+				query = `query=${query}`;
+
+				url += query;
 
 				this.$http.get(url).then(res => {
+					let body = res.body;
 
+					//increment the page counter by one
+					this.page++;
+					this.$emit('success', body);
 				}).catch(err => {
-
+					let body = err.body;
 				});
 			}
 		}
