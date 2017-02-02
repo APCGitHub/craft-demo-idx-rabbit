e25cb9e68513ad84491a2e66bd9fce6cce059151
Andrew Clinton <ddrew.clinton@gmail.com>
2017-02-02 15:37:45 -0500

    bootstrap for all the listings directory files

---
+++ resources/assets/js/components/listings/bootstrap.js
@@ -0,0 +1,10 @@
+/* Inline template logics */
+require('./lead.form');
+require('./listings.results');
+require('./listings.show');
+
+/* Vue components */
+Vue.component('listingMap', require('./ListingMap.vue'));
+Vue.component('listingFav', require('./ListingFav.vue'));
+Vue.component('listingsMap', require('./ListingsMap.vue'));
+Vue.component('listing', require('./Listing.vue'));
